b332ecfa15d7ee9f1af1cdf63e28c8d9dccc3d8f
JangKroed <didlsdydgh@gmail.com>
2022-11-05 14:28:38 +0900

    #10

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import sequelize from './db/config/connection';
+import express from "express";
+import dotenv from "dotenv";
+import sequelize from "./db/config/connection";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }), express.Router);
 
-
-app.get('/', (req, res, next)=>{
-    res.json({
-        message: "SUCCESS"
-    });
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "SUCCESS",
+  });
 });
 
-app.listen(PORT, async() => {
-    console.log('SERVER RUNNING ON PORT: ', PORT);
+app.listen(PORT, async () => {
+  console.log("SERVER RUNNING ON PORT: ", PORT);
 
-    try {
-        await sequelize.authenticate();
-        console.log('DB CONNECTED');
-    } catch (error) {
-        console.error(error);
-        console.log('DB CONNECTION FAIL');
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("DB CONNECTED");
+  } catch (error) {
+    console.error(error);
+    console.log("DB CONNECTION FAIL");
+  }
 });
