558d4adc3419324c328fbeb73aacc4038501b741
Monggle88 <fmonggle88@gmail.com>
2022-11-05 15:12:46 +0900

    Post 기능구현 밑작업

---
+++ src/api/controllers/post.ts
@@ -0,0 +1,4 @@
+import Post from '../../services/post';
+
+class PostController {}
+export default new PostController();
--- src/api/routes/post.ts
+++ src/api/routes/post.ts
@@ -1,8 +1,46 @@
 import { Router } from 'express';
+import Post from '../controllers/post';
 
 const router = Router();
 
-router.route('/');
-router.route('/:postId').get().put().delete();
+/**
+ * FIND ALL POST
+ * request:
+ * response: { data: [...{ postId, userId, nickname, title,
+ *                              createdAt, updatedAt, likes(:int) }] }
+ */
+router.route('/').get().post();
+
+/**
+ * CREATE POST
+ * request header: { Authorization: "Bearer accessToken", Refreshtoken: "refreshToken" }
+ * request: { title, content }
+ * response: { message }
+ */
+router.route('/').post();
+
+/**
+ * FIND POST BY POSTID
+ * request:
+ * response: { data: { postId, userId, nickname, title,
+ *                            content, createdAt, updatedAt } }
+ */
+router.route('/:postId').get();
+
+/**
+ * UPDATE POST
+ * request header: { Authorization: "Bearer accessToken", Refreshtoken: "refreshToken" }
+ * request: { title, content }
+ * response: { message }
+ */
+router.route('/:postId').put();
+
+/**
+ * DELETE POST
+ * request header: { Authorization: "Bearer accessToken", Refreshtoken: "refreshToken" }
+ * request:
+ * response: { message }
+ */
+router.route('/:postId').delete();
 
 export default router;
