7857ea2a830cecc6bc3e455360e697b705911b78
Monggle88 <fmonggle88@gmail.com>
2022-11-05 17:44:09 +0900

    post 기능구현

--- src/api/routes/post.ts
+++ src/api/routes/post.ts
@@ -3,6 +3,8 @@ import { Where } from 'sequelize/types/utils';
 import Users from '../../db/models/user';
 // import Post from '../controllers/post';
 import Post from '../../db/models/post';
+import { number } from 'joi';
+import Posts from '../../db/models/post';
 
 const router = Router();
 
@@ -13,19 +15,23 @@ const router = Router();
  *                              createdAt, updatedAt, likes(:int) }] }
  */
 router
-    .route('/')
-    .get(async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const posts = await Post.findAll({
-                order: ['createdAt', 'DESC'],
-            });
+  .route('/')
+  .get(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const posts = await Post.findAll({
+        order: [['createdAt', 'DESC']],
+        include: {
+          model: Users,
+          attributes: ['name'],
+        },
+      });
 
-            res.status(200).json({ data: posts });
-        } catch (error) {
-            console.log(error);
-            res.sendStatus(400);
-        }
-    });
+      res.status(200).json({ data: posts });
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(400);
+    }
+  });
 
 /**
  * CREATE POST
@@ -34,24 +40,31 @@ router
  * response: { message }
  */
 router
-    .route('/')
-    .post(async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const userId: number = 1;
-            const name: string = 'json';
-            const { title, content } = req.body;
+  .route('/')
+  .post(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      interface PorstForm {
+        title: string;
+        content: string;
+      }
 
-            const post = await Post.create({
-                userId,
-                name,
-                title,
-                content,
-            });
-        } catch (error) {
-            console.log(error);
-            res.sendStatus(400);
-        }
-    });
+      const userId: number = 1;
+      const { title, content }: PorstForm = req.body;
+
+      const post = {
+        userId,
+        title,
+        content,
+      };
+
+      await Post.create(post);
+
+      res.status(201).json({ msg: 'SUCCESS' });
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(400);
+    }
+  });
 
 /**
  * FIND POST BY POSTID
@@ -59,7 +72,28 @@ router
  * response: { data: { postId, userId, nickname, title,
  *                            content, createdAt, updatedAt } }
  */
-router.route('/:postId').get();
+router
+  .route('/:postId')
+  .get(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { postId } = req.params;
+
+      const post = await Post.findOne({
+        where: { postId },
+        include: {
+          model: Users,
+          attributes: ['name'],
+        },
+      });
+
+      if (!post) throw new Error(`Can not found post: ${postId}`);
+
+      res.status(200).json({ data: post });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ msg: `${error}` });
+    }
+  });
 
 /**
  * UPDATE POST
@@ -67,7 +101,30 @@ router.route('/:postId').get();
  * request: { title, content }
  * response: { message }
  */
-router.route('/:postId').put();
+router
+  .route('/:postId')
+  .put(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = 1;
+      const { postId } = req.params;
+      const { title, content } = req.body;
+
+      // is your Post?
+      const isMine = await Post.findOne({ where: { userId, postId } });
+      if (isMine === null) throw new Error(`Is not your post : ${req.params}`);
+
+      const newPost = { title, content };
+
+      await Post.update(
+        { ...newPost, updatedAt: new Date().toLocaleString() },
+        { where: { postId } },
+      );
+
+      res.status(201).json({ msg: 'SUCCESS' });
+    } catch (error) {
+      res.sendStatus(400);
+    }
+  });
 
 /**
  * DELETE POST
@@ -75,6 +132,23 @@ router.route('/:postId').put();
  * request:
  * response: { message }
  */
-router.route('/:postId').delete();
+router
+  .route('/:postId')
+  .delete(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = 2;
+      const { postId } = req.params;
+
+      const isMine = await Post.findOne({ where: { userId, postId } });
+      if (isMine === null) throw new Error(`Is not your post : ${postId}`);
+
+      await Posts.destroy({ where: { postId } });
+
+      res.status(200).json({ msg: 'SUCCESS' });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ msg: `${error}` });
+    }
+  });
 
 export default router;
